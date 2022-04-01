adaa26e8eb6aa797404c4ed9a48ab71e8d8a50b1
pc-test <39305379+pc-test@users.noreply.github.com>
2022-04-01 11:34:07 +0200

    Add files via upload

---
+++ GitScripts/SimpleWebScript/Action.c
@@ -0,0 +1,35 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "TLS1.2");
+	
+	lr_start_transaction("Homepage");
+	
+	web_reg_find("Text={searchTerm}", LAST);
+
+	web_url("www.hpe.com", 
+		"URL=https://www.hpe.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Mode=HTTP", 
+		LAST);
+
+	lr_end_transaction("Homepage", LR_AUTO);
+
+
+	lr_think_time(10);
+
+
+	lr_start_transaction("Login");
+
+	web_url("www.hpe.com", 
+		"URL=https://www.hpe.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Mode=HTTP", 
+		LAST);
+
+	lr_end_transaction("Login", LR_AUTO);
+
+	return 0;
+}
